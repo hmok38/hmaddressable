05b90b3448f9865713414d00bdd6854a3fa41fe1
hmok <hmok@163.com>
2023-03-22 16:41:31 +0800

    覆写完成

--- Assets/Samples/Test/Scripts/HMAddressableTest.cs
+++ Assets/Samples/Test/Scripts/HMAddressableTest.cs
@@ -105,6 +105,7 @@ public class HMAddressableTest : MonoBehaviour
         {
           
             HMAddressableManager.ReleaseRes(capsulePath);
+            HMAddressableManager.ReleaseRes(spherePath);
 
         }
         
